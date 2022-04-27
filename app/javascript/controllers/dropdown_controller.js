df88114eb9f28aacf63ffaad6afecf9c06bc5dfc
ADK <web.dev.adk@gmail.com>
2022-04-27 12:32:28 +0600

    fixed dropdown form

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dropdown"
+export default class extends Controller {
+
+  connect() { console.log("dropdown controller connected") }
+
+  usePreventDefault(event) {
+    event.stopImmediatePropagation()
+  }
+}
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import DebounceController from "./debounce_controller.js"
 application.register("debounce", DebounceController)
 
+import DropdownController from "./dropdown_controller.js"
+application.register("dropdown", DropdownController)
+
 import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
